6e7e54c1c285487921e0ffad37a7477ddf8e6141
taneesh-kumar <heyitstaneesh@gmail.com>
2024-11-22 08:33:34 +0530

    1

--- LIst_direc.py
+++ LIst_direc.py
@@ -4,5 +4,9 @@ directory_path = "/users/hp/OneDrive/Documents/"
 
 contents = os.listdir(directory_path)
 
+# If you want to list files in a vertical manner
 for item in contents:
     print(item)
+
+# If you want to list files in a horizontal manner
+print(contents)
