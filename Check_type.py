e2006fc2c72e74aebdf6beb326923c47fff0a263
taneesh-kumar <heyitstaneesh@gmail.com>
2024-11-28 11:09:39 +0530

    day 2

---
+++ Check_type.py
@@ -0,0 +1,20 @@
+t1 = 5
+print (type(t1))
+
+t2 = 5.0
+print (type(t2))
+
+t3 = "Hello"
+print (type(t3))
+
+t4 = [1, 2, 3]
+print (type(t4))
+
+t5 = (1, 2, 3)
+print (type(t5))
+
+t6 = {"name": "John", "age": 30}
+print (type(t6))
+
+t7 = {"name": "John", "age": 30}
+print (t6 is t7)
---
+++ Greetings.py
@@ -0,0 +1,11 @@
+name = input("Enter your username :")
+
+print("Good morning! "+name)
+
+# or 
+
+print(f"Good morning! {name}")
+
+# for raw string
+
+print(r"Good morning! {name}")
